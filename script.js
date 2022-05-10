f72bfd98fd829d3cd2295eb96ac1156ffc3b3e6d
Geoffrey Coyne <gmancoyne@gmail.com>
2022-05-10 18:26:26 -0400

    recovered stylesheet and js

---
+++ script.js
@@ -0,0 +1,19 @@
+if (document.cookie != "") {
+    document.getElementById("stylesheet-link").href = document.cookie;
+} else {
+    document.cookie = "style-lightmode.css";
+    //document.getElementById("stylesheet-link").href = document.cookie;
+}
+function switchLights(){
+    if (document.cookie == "style-lightmode.css") {
+        document.cookie = "style-darkmode.css";
+        document.getElementById("stylesheet-link").href = document.cookie;
+    } else if (document.cookie == "style-darkmode.css") {
+        document.cookie = "style-lightmode.css";
+        document.getElementById("stylesheet-link").href = document.cookie;
+    } else {
+        document.cookie = "style-darkmode.css";
+        document.getElementById("stylesheet-link").href = document.cookie;
+    }
+
+}
