ab9366afaa5e433d05b3385870e831f4a815e819
Geoffrey Coyne <gmancoyne@gmail.com>
2022-05-10 21:50:27 -0400

    Tryed to fix cookie again

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ if (document.cookie != "") {
     const themeCookieValue = document.cookie.split('; ').find(row => row.startsWith('theme=')).split('=')[1];
     document.getElementById("stylesheet-link").href = themeCookieValue;
 } else {
-    document.cookie = "theme=style-lightmode.css;";
+    document.cookie = "theme=style-lightmode.css; SameSite=Strict;";
     const themeCookieValue = document.cookie.split('; ').find(row => row.startsWith('theme=')).split('=')[1];
 }
 function switchLights(){
     const themeCookieValue = document.cookie.split('; ').find(row => row.startsWith('theme=')).split('=')[1];
     if (themeCookieValue == "style-lightmode.css") {
-        document.cookie = "theme=style-darkmode.css;";
+        document.cookie = "theme=style-darkmode.css; SameSite=Strict;";
         document.getElementById("stylesheet-link").href = "style-darkmode.css";
     } else if (themeCookieValue == "style-darkmode.css") {
-        document.cookie = "theme=style-lightmode.css;";
+        document.cookie = "theme=style-lightmode.css; SameSite=Strict;";
         document.getElementById("stylesheet-link").href = "style-lightmode.css";
     } else {
         document.cookie = "theme=style-darkmode.css;";
-        document.getElementById("stylesheet-link").href = "style-lightmode.css";
+        document.getElementById("stylesheet-link").href = "theme=style-lightmode.css SameSite=Strict;";
     }
 
 }
